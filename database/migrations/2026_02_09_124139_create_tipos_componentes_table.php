e8b5a6cca29d0129d94a004c098120dbcfa1b551
12Sergi0 <sergi@BLAME1.localdomain>
2026-02-09 14:03:01 +0100

    Añado migración tipos_componente

---
+++ database/migrations/2026_02_09_124139_create_tipos_componentes_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTiposComponentesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tipos_componente', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre', 100);
+            $table->text('descripcion')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tipos_componentes');
+    }
+}
